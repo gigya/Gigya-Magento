72086a850f2120ca3a63d0080462a3f81f563eca
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2012-10-14 18:05:27 +0200

    some more work

--- js/gigya/gigya.js
+++ js/gigya/gigya.js
@@ -1,3 +1,19 @@
+/*
+ *gigya functions
+ */
+var gigyaFunctions = gigyaFunctions || {};
+gigyaFunctions.login = function (responce) {
+  console.log(responce);
+};
+
+/*
+ * register events
+ */
+if (typeof gigya !== 'undefined') {
+  gigya.services.socialize.addEventHandlers({onLogin:gigyaFunctions.login});
+}
+
+
 document.observe("dom:loaded", function() {
   if (typeof gigyaSettings !== 'undefined'){
     gigya.socialize.showLoginUI(gigyaSettings.login);
