676db2b4b75d516b6c84cfd49ca61e87ae7bbf76
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2013-07-31 15:53:27 +0300

    added data center config UI

---
+++ app/code/community/Gigya/Social/Model/Config/Source/Datacenter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Yaniv Aran-Shamir
+ * Date: 7/31/13
+ * Time: 3:32 PM
+ */
+
+class Gigya_Social_Model_Config_Source_Datacenter {
+public function toOptionArray(){
+  return array(
+    array('value' => 'us1.gigya.com', 'label' => 'US Data Center'),
+    array('value' => 'eu1.gigya.com', 'label' => 'EU Data Center'),
+  );
+}
+}
