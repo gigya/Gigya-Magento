2c09c3b3dbd7a3154e59e50290ba2696e0645bd0
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2014-07-08 12:24:12 +0300

    fixed missing warnings 34313

--- app/code/community/Gigya/Social/Model/Config/Backend/UserMode.php
+++ app/code/community/Gigya/Social/Model/Config/Backend/UserMode.php
@@ -16,9 +16,26 @@ class Gigya_Social_Model_Config_Backend_UserMode extends Mage_Core_Model_Config_
             if (!$helper->utils->isRaaS()) {
                 Mage::throwException(Mage::helper('adminhtml')->__("Gigya's Registration-as-a-Service (RaaS) is currently not part of your site package.
             Please contact your Gigya account manager to activate the package."));
+            } else {
+                $data = $this->getData();
+                $raasConfig = $data['gigya_raas_conf']['fields'];
+                if (!$this->checkNotEmptyRaas($raasConfig)) {
+                    Mage::throwException(Mage::helper('adminhtml')->__("Screen set IDs can not be empty"));
+                }
             }
         }
 
+    }
+
+    private function checkNotEmptyRaas($config)
+    {
+        $required = array('WebScreen', 'MobileScreen', 'LoginScreen', 'RegisterScreen', 'ProfileWebScreen', 'ProfileMobileScreen');
+        foreach ($required as $field) {
+            if (empty($config[$field]['value'])) {
+                return false;
+            }
+       }
+       return true;
     }
 
 
