b4cb4df347026a1b3a54d6c89106803ef3404783
guy-gigya <guy.avivi@gigya-inc.com>
2015-01-26 18:14:21 +0200

    #41662:RAAS: Login with new TW identity, login to Gigya and then logout fix

--- app/code/community/Gigya/Social/Block/Adminhtml/Version.php
+++ app/code/community/Gigya/Social/Block/Adminhtml/Version.php
@@ -6,7 +6,7 @@
  * Time: 11:48 AM
  */
 
-class Gigya_Social_Block_Adminhtml_Version extends Mage_Adminhtml_Block_System_Config_Form_Field {
+class Gigya_Social_Block_Adminhtml_version extends Mage_Adminhtml_Block_System_Config_Form_Field {
   protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
   {
     return (string) Mage::helper('Gigya_Social')->getExtensionVersion();
